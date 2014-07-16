eac0b962fcced090e1c4272fa6c83019e588bb8e
johnnic431 <johnnic431@gmail.com>
2014-07-16 18:44:56 -0500

    Logger

---
+++ .revolutOS/APIS/Logger.lua
@@ -0,0 +1,23 @@
+--Logger for revolutOS
+
+verison=0.1;
+build=1;
+file=".revolutOS/log.txt";
+sTime=0;
+ln="\n";
+
+fs.makeDir(".revolutOS");
+
+new=function()
+	sTime=os.time();
+	f=fs.open(file,"w");
+	f.write("File beginning at "..tostring(sTime)..ln);
+	f.close();
+end
+
+log=function(message)
+	nTime=os.time();
+	f=fs.open(file,"a");
+	f.write("["..nTime-sTime.."] "..message..ln);
+	f.close();
+end
