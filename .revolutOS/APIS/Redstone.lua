c1cb9b71945b137f20c3311c5332c380e84c452a
johnnic431 <johnnic431@gmail.com>
2014-07-16 18:45:32 -0500

    Redstone is the application base for revolutOS

--- .revolutOS/APIS/Redstone.lua
+++ .revolutOS/APIS/Redstone.lua
@@ -3,8 +3,10 @@
 version=0.1;
 build=1;
 
-function init(self,info)
-	os.loadAPI(apisPath.."loadAPI");
+apisNeeded={"Screen"};
+
+function init(this,name,info)
+	os.loadAPI("loadAPI");
 	load=loadAPI.loadAPI;
 	apisPath=loadAPI.apiPath;
 	local env=getfenv();
@@ -16,8 +18,10 @@ function init(self,info)
 		end
 	end
 	new={};
-	new.view=Screen:new();
+	new.view=nil;
 	new.eventHandlers={};
+	new.isTerminatable=true;
+	new.name=name;
 	inf=info or {};
 	for k,v in pairs(inf) do
 		if new[k] then
@@ -30,4 +34,29 @@ function init(self,info)
 		end
 	end
 	return setmetatable(new,this);
+end
+
+function addEventHandler(self,eHandler,typef)
+	 if typeof then
+		eventHandlers:insert({handler=eHandler,typeof=typef});
+	 else
+		eventHandlers:insert({handler=eHandler,typeof="all"});
+	 end
+end
+
+function start(this)
+	this.view:draw();
+	t=coroutine.create((function() 
+		e={os.pullEvent()};
+		for t,y in pairs(this.eventHandlers) do
+			if y.typeof==e[1] or y.typeof=="all" then
+				y.handler(e);
+			end
+		end
+	end));
+	coroutine.resume(t);
+end
+
+function redraw(this)
+	this.view:draw();
 end
